0693196c924c90cd129935b21fc34e8e6026a7c2
Jack B <jack.github@gmail.com>
2015-10-31 03:54:24 -0400

    Fixed the paths for the audio files.

--- source/Threads/AudioThread.java
+++ source/Threads/AudioThread.java
@@ -16,7 +16,7 @@ public class AudioThread extends Thread {
 
     public void run() {
         try {
-            FileInputStream audioInputStream = new FileInputStream("D:\\Programming\\Programs\\IdeaProjects\\UndercoverActive\\source\\Audio\\AudioFiles\\Music.mp3");
+            FileInputStream audioInputStream = new FileInputStream(getClass().getResource("../Audio/AudioFiles/Music.mp3").getPath());
             final Player audioPlayer = new Player(audioInputStream);
             audioPlayer.play();
             Timer timer = new Timer(1000 / 60, new ActionListener() {
