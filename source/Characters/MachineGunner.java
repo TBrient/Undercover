500cefc9a9e12725b4d6faf7fdbf41330b9b6989
kanming_xu <kanmingwx@gmail.com>
2015-10-31 18:29:59 -0400

    Fixed the paths for the audio files.

---
+++ source/Characters/MachineGunner.java
@@ -0,0 +1,10 @@
+package source.Characters;
+
+/**
+ * Created by kanming_xu on 10/31/15.
+ */
+public class MachineGunner extends Character {
+    public MachineGunner() {
+        super(100, 50, 20, 50, 100);
+    }
+}
