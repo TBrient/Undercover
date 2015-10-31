bc6b0e4c7262280d230f49b0c8146e8835e4f8e2
kanming_xu <kanmingwx@gmail.com>
2015-10-30 23:03:05 -0400

    YE?

--- Begin.java
+++ Begin.java
@@ -33,104 +33,107 @@ public class Begin {
             public void keyPressed(KeyEvent keyEvent) {
 
 
+                if (characterChosen == 0) {
+                    if (keyEvent.getKeyCode() == 37) {
+                        if (characterChosen == 0) {
+                            if (Location == 1) {
+                                Location = 4;
+                            } else {
+                                Location -= 1;
+                            }
+                        }
+                    } else if (keyEvent.getKeyCode() == 39) {
+                        if (characterChosen == 0) {
+                            if (Location == 4) {
+                                Location = 1;
+                            } else {
+                                Location += 1;
+                            }
+                        }
+                    } else if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
 
-                if (keyEvent.getKeyCode() == 37) {
-                    if (characterChosen == 0) {
-                        if (Location == 1) {
-                            Location = 4;
-                        } else {
-                            Location -= 1;
+
+                        if (enter == 1) {
+                            switch (Location) {
+                                case (1): {
+                                    System.out.println("You Selected Rifleman");
+                                    character = new Character(1);
+                                    characterChosen = 1;
+                                    break;
+                                }
+                                case (2): {
+                                    System.out.println("You selected Sniper");
+                                    character = new Character(2);
+                                    characterChosen = 1;
+                                    break;
+                                }
+                                case (3): {
+                                    System.out.println("You selected Machine Gunner");
+                                    character = new Character(3);
+                                    characterChosen = 1;
+                                    break;
+                                }
+                                case (4): {
+                                    System.out.println("You selected Scout");
+                                    character = new Character(4);
+                                    characterChosen = 1;
+                                    break;
+
+                                }
+                            }
+                        } else if (enter == 0) {
+                            enter = 1;
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            Screen screen1 = new Screen(2);
+
+                        }
+                    } else if (keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
+                        if (enter == 1) {
+                            switch (Location) {
+                                case (1): {
+                                    System.out.println("You Selected Rifleman");
+                                    character = new Character(1);
+                                    characterChosen = 1;
+                                    break;
+                                }
+                                case (2): {
+                                    System.out.println("You selected Sniper");
+                                    character = new Character(2);
+                                    characterChosen = 1;
+                                    break;
+                                }
+                                case (3): {
+                                    System.out.println("You selected Machine Gunner");
+                                    character = new Character(3);
+                                    characterChosen = 1;
+                                    break;
+                                }
+                                case (4): {
+                                    System.out.println("You selected Scout");
+                                    character = new Character(4);
+                                    characterChosen = 1;
+                                    break;
+
+                                }
+                            }
+                        } else if (enter == 0) {
+                            enter = 1;
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            Screen screen1 = new Screen(2);
+
                         }
                     }
-                } else if (keyEvent.getKeyCode() == 39) {
-                    if (characterChosen == 0) {
-                        if (Location == 4) {
-                            Location = 1;
-                        } else {
-                            Location += 1;
-                        }
-                    }
-                } else if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
-
-
-
-                    if (enter == 1) {
-                        switch (Location) {
-                            case (1): {
-                                System.out.println("You Selected Rifleman");
-                                character = new Character(1);
-                                characterChosen = 1;
-                                break;
-                            }
-                            case (2): {
-                                System.out.println("You selected Sniper");
-                                character = new Character(2);
-                                characterChosen = 1;
-                                break;
-                            }
-                            case (3): {
-                                System.out.println("You selected Machine Gunner");
-                                character = new Character(3);
-                                characterChosen = 1;
-                                break;
-                            }
-                            case (4): {
-                                System.out.println("You selected Scout");
-                                character = new Character(4);
-                                characterChosen = 1;
-                                break;
-
-                            }
-                        }
-                    } else if (enter == 0) {
-                        enter = 1;
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        Screen screen1 = new Screen(2);
-
-                    }
-                } else if (keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
-                    if (enter == 1) {
-                        switch (Location) {
-                            case (1): {
-                                System.out.println("You Selected Rifleman");
-                                character = new Character(1);
-                                characterChosen = 1;
-                                break;
-                            }
-                            case (2): {
-                                System.out.println("You selected Sniper");
-                                character = new Character(2);
-                                characterChosen = 1;
-                                break;
-                            }
-                            case (3): {
-                                System.out.println("You selected Machine Gunner");
-                                character = new Character(3);
-                                characterChosen = 1;
-                                break;
-                            }
-                            case (4): {
-                                System.out.println("You selected Scout");
-                                character = new Character(4);
-                                characterChosen = 1;
-                                break;
-
-                            }
-                        }
-                    } else if (enter == 0) {
-                        enter = 1;
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        Screen screen1 = new Screen(2);
-
-                    }
+                } else if (characterChosen == 1){
+                    // YEYEYEYEYE
+                    Screen screen1 = new Screen(1);
                 }
             }
 
