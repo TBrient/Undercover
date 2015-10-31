c1ef524840030337e46a812bff1674f336a80885
Jack B <jack.github@gmail.com>
2015-10-31 08:40:27 -0400

    Added move method

--- source/Characters/MenuCharacter.java
+++ source/Characters/MenuCharacter.java
@@ -1,10 +1,17 @@
 package source.Characters;
 
+import java.awt.event.KeyEvent;
+
 /**
  * Created by Jack on 10/30/2015.
  */
 public class MenuCharacter extends Character {
     public MenuCharacter(int healthPoints, int damage, int fireRate, int speed, int mana) {
         super(healthPoints, damage, fireRate, speed, mana);
+    }
+
+    @Override
+    public void move(KeyEvent e) {
+
     }
 }
