2474c62704f2e68cd35f9d0bfc0b6d46729d1eb3
kanming_xu <kanmingwx@gmail.com>
2015-10-31 18:30:27 -0400

    Fixed the paths for the audio files.

---
+++ source/Characters/Rifleman.java
@@ -0,0 +1,10 @@
+package source.Characters;
+
+/**
+ * Created by kanming_xu on 10/31/15.
+ */
+public class Rifleman extends Character {
+    public Rifleman() {
+        super(100, 100, 100, 100, 100);
+    }
+}
